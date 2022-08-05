48c2764a8af4d61ed68a58844d4a88ef31a16b0f
Arpit Agarwal <arpit2000ag@gmail.com>
2022-08-05 22:29:27 +0530

    Changed index

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Facebook from './Facebook';
+// import Facebook from './Facebook';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Facebook />
+    <App />
+    {/* <Facebook /> */}
   </React.StrictMode>
 );
 
